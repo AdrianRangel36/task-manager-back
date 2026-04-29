9152728a4c1847c78b7c8176f20f96e053a80525
AdrianRangel36 <adrianrvdev@gmail.com>
2026-04-29 16:34:35 -0600

    feature: configure project and install prisma

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { TeamMembersModule } from './team-members/team-members.module';
+import { TeamModule } from './team/team.module';
+import { TasksModule } from './tasks/tasks.module';
+
+@Module({
+  imports: [UsersModule, TeamMembersModule, TeamModule, TasksModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
