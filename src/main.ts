0802040469d8a22410984ab44975076923b2416c
AdrianRangel36 <adrianrvdev@gmail.com>
2026-05-30 20:15:55 -0600

    refactor: reorganize CORS and validation pipe setup in main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,19 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist:true,
-    transform:true
-  }))
+
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
